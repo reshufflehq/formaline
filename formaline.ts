d0f5f3681e6a3b23861bdfeea2cd0096667f8632
Avner Braverman <avner@binaris.com>
2020-11-15 12:01:53 -0800

    Email field

--- formaline.ts
+++ formaline.ts
@@ -11,6 +11,17 @@
                          '(\\.[\\w\\-]+)*(\\/[\\.\\w\\-]+)*\\/?' +
                          '(\\?[\\w\\-]+=[^&]*(&[\\w\\-]+=[^&]*)*)?$')
 
+  const EMAIL = new RegExp(
+    '^(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]' +
+    '+)*|"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7' +
+    'f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*")@(?:(?:[a-z0-9](?:[a-z' +
+    '0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[' +
+    '0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4]' +
+    '[0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08' +
+    '\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b' +
+    '\\x0c\\x0e-\\x7f])+)\\])$'
+  )
+
   interface Options {
     onValidate?: (name: string, value: string) => Promise<string | void>
     validateEndpoint?: string
@@ -260,6 +271,13 @@
       return $input
     }
 
+    // Fields /////////////////////////////////////////////
+
+    public email(name: string, label: string) {
+      this.stage(this.input('email', EMAIL, name, label))
+      return this
+    }
+
     public phone(name: string, label: string, validate = false) {
       if (validate && !this.canValidate) {
         throw new Error('No means to validate: ' +
@@ -280,6 +298,8 @@
       this.stage(this.input('text', /^(?!\s*$).+/, name, label))
       return this
     }
+
+    // Accessors //////////////////////////////////////////
 
     public getData() {
       const data: Data = { fields: {}, codes: {}, cookies: this.cookies }
